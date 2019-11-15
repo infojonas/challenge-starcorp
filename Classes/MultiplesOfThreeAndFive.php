49401742a478d2e887d691cfd2ce15cf6ac43dfd
Thais Molina <thaismolina@gmail.com>
2019-11-15 19:32:28 -0300

    Updated calculator of MultiplesOfThreeAndFive

--- Classes/MultiplesOfThreeAndFive.php
+++ Classes/MultiplesOfThreeAndFive.php
@@ -7,20 +7,45 @@ namespace Classes;
  */
 class MultiplesOfThreeAndFive extends Numbers
 {
+    public $multipleOfThreeAndFive;
+
     /**
      * MultiplesOfThreeAndFive constructor.
      */
     public function __construct()
     {
+        self::calculateMultipleOfThreeAndFive();
         self::setNumbers([]);
-        self::setFrom(15);
+        self::setFrom($this->multipleOfThreeAndFive);
         self::setTo(100);
     }
 
     public function countNumbers()
     {
-        for ($i = $this->from; $i <= $this->to; $i += 15) {
+        for ($i = $this->from; $i <= $this->to; $i += $this->multipleOfThreeAndFive) {
             $this->arrayNumbers[$i] = 'StarCorpianos';
         }
+    }
+
+    public function calculateMultipleOfThreeAndFive() {
+        $arrayMMC = [];
+        $arrayDividers = [2,3,5];
+        $result = 1;
+
+        foreach ($arrayDividers as $divider) {
+            if (3 % $divider === 0) {
+                $arrayMMC[] = $divider;
+            }
+
+            if (5 % $divider === 0) {
+                $arrayMMC[] = $divider;
+            }
+        }
+
+        foreach ($arrayMMC as $mmc) {
+            $result *= $mmc;
+        }
+
+        $this->multipleOfThreeAndFive = $result;
     }
 }
